c2526c982ecdce240c1420edd47181e2ed1b84ff
Mugil <mugil.choi@gmail.com>
2023-11-07 23:42:31 -0500

    fixed aws issues

--- react-app/src/store/song.js
+++ react-app/src/store/song.js
@@ -3,7 +3,6 @@ const GET_ALL_SONGS = "songs/GET_ALL_SONGS";
 const GET_SONG_BY_ID = "songs/GET_SONG_BY_ID";
 const UPLOAD_SONG = "songs/UPLOAD_SONG";
 const DELETE_SONG = "songs/DELETE_SONG";
-const UPDATE_SONG = "songs/UPDATE_SONG";
 
 const getAllSongs = (songs) => ({
   type: GET_ALL_SONGS,
@@ -25,11 +24,6 @@ const deleteSong = (songId) => ({
   payload: songId,
 });
 
-const updateSong = (updatedSong) => ({
-  type: UPDATE_SONG,
-  payload: updatedSong,
-});
-
 export const fetchAllSongs = () => async (dispatch) => {
   const response = await fetch("/api/songs/");
   if (response.ok) {
@@ -46,24 +40,31 @@ export const fetchSongById = (songId) => async (dispatch) => {
   }
 };
 
-export const createSong = (formData) => async (dispatch) => {
+export const createSong = (songData) => async (dispatch) => {
   try {
     const response = await fetch("/api/songs/", {
       method: "POST",
-      body: formData,
+      // headers: {
+      //   "Content-Type": "application/json",
+      // },
+      body: songData,
     });
 
-    if (response.ok) {
-      const data = await response.json();
-      dispatch(uploadSong(data));
-    } else {
-      const errorData = await response.json();
-      console.error("Error creating song:", errorData);
-      return { errors: errorData.errors || ["An error occurred while creating the song."] };
+    if (!response.ok) {
+      if (response.status === 400) {
+        const data = await response.json();
+        return { errors: data.errors };
+      } else {
+        throw new Error("Server error");
+      }
     }
+
+    const data = await response.json();
+    dispatch(uploadSong(data));
+    return data;
   } catch (error) {
     console.error("Network error:", error);
-    return { errors: ["A network error occurred while creating the song."] };
+    return { errors: ["Network error"] };
   }
 };
 
@@ -74,22 +75,6 @@ export const removeSong = (songId) => async (dispatch) => {
 
   if (response.ok) {
     dispatch(deleteSong(songId));
-  } else {
-  }
-};
-
-export const editSong = (songId, updatedData) => async (dispatch) => {
-  const response = await fetch(`/api/songs/${songId}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(updatedData),
-  });
-
-  if (response.ok) {
-    const data = await response.json();
-    dispatch(updateSong(data));
   } else {
   }
 };
@@ -109,14 +94,6 @@ export default function songsReducer(state = initialState, action) {
         ...state,
         songs: state.songs.filter((song) => song.id !== action.payload),
       };
-    case UPDATE_SONG:
-      const updatedIndex = state.songs.findIndex((song) => song.id === action.payload.id);
-      if (updatedIndex !== -1) {
-        const updatedSongs = [...state.songs];
-        updatedSongs[updatedIndex] = action.payload;
-        return { ...state, songs: updatedSongs };
-      }
-      return state;
     default:
       return state;
   }
