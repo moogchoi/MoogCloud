3dae98b211e97795b53b558c53cec2ac0a0f9f9c
Mugil <mugil.choi@gmail.com>
2023-11-09 21:56:18 -0500

    added csrf to upload comment

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -5,6 +5,14 @@ from app.forms import CommentForm
 
 comment_routes = Blueprint('comments', __name__)
 
+def validation_errors_to_error_messages(validation_errors):
+  error_messages = []
+  for field, errors in validation_errors.items():
+    for error in errors:
+      error_messages.append(f"{field}: {error}")
+  return error_messages
+
+
 # get all comments for a song
 @comment_routes.route('/<int:song_id>', methods=['GET'])
 def get_comments(song_id):
@@ -15,22 +23,18 @@ def get_comments(song_id):
 @comment_routes.route('/<int:song_id>', methods=['POST'])
 @login_required
 def add_comment(song_id):
-    form = CommentForm()
+    form = CommentForm(request.form)
     form['csrf_token'].data = request.cookies['csrf_token']
-
     if form.validate_on_submit():
-        new_comment = Comment(
+        comment = Comment(
             user_id=current_user.id,
             song_id=song_id,
             text=form.data['text']
         )
-
-        db.session.add(new_comment)
+        db.session.add(comment)
         db.session.commit()
-
-        return new_comment.to_dict()
-
-    return {'errors': form.errors}, 400
+        return comment.to_dict(), 201
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 400
 
 # update a user's comment on a song
 @comment_routes.route('/<int:comment_id>', methods=['PUT'])
