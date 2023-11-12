cf157608d83e4bde4f23508348ed16e874d9cfda
Mugil <mugil.choi@gmail.com>
2023-11-11 21:05:08 -0500

    navbar styling and functionality

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,27 +1,72 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import ProfileButton from './ProfileButton';
+import { logout } from '../../store/session';
 import './Navigation.css';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
+import github from '../Images/github.svg';
+import logo from '../Images/logo.png'
 
 function Navigation({ isLoaded }){
 	const sessionUser = useSelector(state => state.session.user);
+	const dispatch = useDispatch();
+
+	const handleLogout = () => {
+    dispatch(logout());
+  };
 
 	return (
-		<div className='nav-container'>
-		<div>
-			<div className='home-button'>
-				<NavLink exact to="/">Home</NavLink>
-			</div>
-			{isLoaded && (
-				<div>
-					<ProfileButton
-						user={sessionUser} />
-				</div>
-			)}
-		</div>
-		</div>
-	);
+    <div className='nav-container'>
+      <div className='left-container'>
+        <div className='logo-container'>
+          <NavLink to="/" id='landing-logo'>
+            <img src={logo} alt="logo" />
+          </NavLink>
+        </div>
+      </div>
+			<div className='github-links'>
+          <div className='github-developer'>
+          <a href="https://github.com/moogchoi">
+            <div className='github'>
+              <img
+								src={github}
+                id="github-logo"
+                alt=""
+              ></img>
+            </div>
+            </a>
+          </div>
+        </div>
+      <div className="right-nav-container">
+        {sessionUser ? (
+          <><button onClick={handleLogout} className="login">
+            Logout
+          </button>
+            <NavLink className="signup" to="/new">
+              Upload
+            </NavLink>
+						<NavLink className="login" to="/current">
+              Manage
+            </NavLink></>
+        ) : (
+          <>
+            <OpenModalButton
+          		buttonText="Log In"
+          		className="login"
+          		modalComponent={<LoginFormModal />}
+        		/>
+            <OpenModalButton
+         			buttonText="Sign Up"
+          		className="signup"
+          		modalComponent={<SignupFormModal />}
+        		/>
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
-
 export default Navigation;
